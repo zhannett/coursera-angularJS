18908225b5feb1637009d8e7ef58f30fcb83ee73
zhannett <janet@hubub.com>
2017-02-12 15:39:17 -0500

    functionality updates for empty text field

--- module3-solution/app.js
+++ module3-solution/app.js
@@ -27,13 +27,16 @@
     menu.found = [];
     menu.startSearch = function() {
       var searchTerm = document.querySelectorAll('input')[0].value.toLowerCase();
-      menu.found = MenuSearchService.getMatchedMenuItems(searchTerm)
-       .then(function(result) {
-          menu.found = result;
-          if (menu.found.length > 0) {
-            menu.title = "Search Results for '" + searchTerm;
-          }
+      if (searchTerm === '') {
+        menu.found = [];
+        menu.title = "Nothing found";
+      } else {
+        menu.found = MenuSearchService.getMatchedMenuItems(searchTerm)
+         .then(function(result) {
+           menu.found = result;
+           menu.title = "Search Results for " + searchTerm;
         });
+      }
     }
     menu.removeItem = function (itemIndex) {
       menu.found.splice(itemIndex, 1);
